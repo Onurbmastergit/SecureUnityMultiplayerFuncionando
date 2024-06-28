09428dffa514d286b95a337dcaca56194a39273f
KvnTakara <kvntakara@gmail.com>
2024-06-28 17:29:34 -0300

    Melhorias ZombieBulker

--- Assets/Scripts/ExplosionScript.cs
+++ Assets/Scripts/ExplosionScript.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 
 public class ExplosionScript : MonoBehaviour
 {
+    [SerializeField] int danoExplosao;
+
     void Start()
     {
         transform.GetComponent<Collider>().enabled = true;
         Invoke("DestroyerMine", 3);
     }
-   void OnTriggerEnter(Collider collider)
-   {
-        if(collider.CompareTag("Zombie"))
-        collider.GetComponent<EnemyStatus>().ReceberDano(collider.GetComponent<EnemyStatus>().vidaAtual);
-   }
 
-   void DestroyerMine()
-   {
-     transform.parent.GetComponent<Mina>().DestroyMine();
-   }
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Zombie"))
+        {
+            collider.GetComponent<EnemyStatus>().ReceberDano(danoExplosao);
+        }
+    }
+
+    void DestroyerMine()
+    {
+        transform.parent.GetComponent<Mina>().DestroyMine();
+    }
 }
